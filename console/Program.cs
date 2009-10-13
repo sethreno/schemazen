89ad5f60464b0c2bc3ee35546b100049d7c0ee2b
sreno <sreno@49102ff3-e64e-284a-8f61-b7e53b302e95>
2009-10-13 20:18:09 +0000

    git-svn-id: svn://localhost/schemanator/trunk@30 49102ff3-e64e-284a-8f61-b7e53b302e95

--- console/Program.cs
+++ console/Program.cs
@@ -1,89 +1,138 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-
+using CommandLine;
+using CommandLine.Text;
 using model;
 
 namespace console
 {
 	class Program {
+        class Options
+        {
+            [Option("c", "conn_string",
+                Required = false,                
+                HelpText = @"Connection string to the db to script.
+
+      Examples: ""Server=localhost;Database=MYDB;Trusted_Connection=True;""
+                ""Server=localhost;Database=MYDB;User Id=user;Password=pass;""
+            ")]
+            public string ConnString = "";
+
+            [Option("a", "conn_appSetting",
+                Required = false,
+                HelpText = @"The key for a .net appSetting that contains
+                        the connection string to the db to script.
+            ")]
+            public string ConnAppSetting = "";
+
+            [Option("d", "output_dir",
+                Required = false,
+                HelpText = @"Output directory for generated scripts.
+                        If this option is used a separate script will be
+                        generated for each object. Scripts are grouped
+                        into sub directories by object type.
+            ")]
+            public string Dir = "";
+
+            [HelpOption(HelpText = "Display this help screen.")]
+            public string GetHelp(){
+                var txt = new HelpText("schemacon - schemanator console");
+                txt.Copyright = new CopyrightInfo("Seth Reno", 2009);
+                txt.AddPreOptionsLine(@"
+Usage: schemacon -c <connection string> [-d <output dir>]
+       schemacon -a <app setting> [-d <output dir>]");
+                txt.AddOptions(this);
+                return txt;
+            }
+        }
+
 		static int Main(string[] args) {
-            Options options = new Options(args);
-            if (!options.Validate()) {
-                foreach (string e in options.Errors) {
-                    Console.WriteLine(e);
-                }
+            var options = new Options();
+            var parser = new CommandLineParser(new CommandLineParserSettings(Console.Error));
+            if (!parser.ParseArguments(args, options)){
                 return -1;
             }
-			
-			// create dir tree
-            Console.WriteLine("creating directory tree");
-			string[] dirs = { "data",  "foreign_keys", "functions", 
-                              "procs", "tables",       "triggers" };
-			foreach (string dir in dirs) {
-				if (!Directory.Exists(options.Dir + "/" + dir)) {
-					Directory.CreateDirectory(options.Dir + "/" + dir);
-				}
-			}
+            if (string.IsNullOrEmpty(options.ConnAppSetting)
+             && string.IsNullOrEmpty(options.ConnString)){
+                 Console.WriteLine("You must specify a connection string or app setting.");
+                 Console.WriteLine("Enter \"schemacon --help\" for more info.");
+                 return -1;
+            }
+
+            if (!String.IsNullOrEmpty(options.ConnAppSetting)) {
+                var appSettings = new System.Configuration.AppSettingsReader();
+                options.ConnString = (string)appSettings.GetValue(options.ConnAppSetting, typeof(string));
+            }
 
             // load the model
             var db = new Database();
             db.Load(options.ConnString);
-                        
+
+            // generate scripts
+            if (!String.IsNullOrEmpty(options.Dir)) {
+                ScriptToDir(options, db);
+            } else {
+                ScriptToOutput(options, db);
+            }
+                       
+            return 0;
+		}
+
+        private static void ScriptToOutput(Options options, Database db) {
+            foreach (Table t in db.Tables) {
+                Console.WriteLine(t.ScriptCreate());
+                Console.WriteLine("GO");                
+            }
+            foreach (ForeignKey fk in db.ForeignKeys) {
+                Console.WriteLine(fk.ScriptCreate());
+                Console.WriteLine("GO");
+            }
+            foreach (Routine r in db.Routines) {
+                Console.WriteLine(r.ScriptCreate());
+                Console.WriteLine("GO");
+            }
+        }
+
+        private static void ScriptToDir(Options options, Database db) {
+            // create dir tree
+            Console.WriteLine("creating directory tree");
+            string[] dirs = { "data",  "foreign_keys", "functions", 
+                              "procs", "tables",       "triggers" };
+            foreach (string dir in dirs) {
+                if (!Directory.Exists(options.Dir + "/" + dir)) {
+                    Directory.CreateDirectory(options.Dir + "/" + dir);
+                }
+            }
+
             Console.WriteLine("scripting tables");
-			foreach (Table t in db.Tables){
+            foreach (Table t in db.Tables) {
                 File.WriteAllText(
                     String.Format("{0}/tables/{1}.sql", options.Dir, t.Name),
                     t.ScriptCreate() + "\r\nGO\r\n"
                 );
-			}
-                        
+            }
+
             Console.WriteLine("scripting foreign keys");
-            foreach (ForeignKey fk in db.ForeignKeys){           
+            foreach (ForeignKey fk in db.ForeignKeys) {
                 File.AppendAllText(
                     String.Format("{0}/foreign_keys/{1}.sql", options.Dir, fk.Table.Name),
                     fk.ScriptCreate() + "\r\nGO\r\n"
                 );
             }
-            			
+
             Console.WriteLine("scripting procs, functions, & triggers");
-            foreach (Routine r in db.Routines)
-            {
+            foreach (Routine r in db.Routines) {
                 string dir = "procs";
                 if (r.Type == "TRIGGER") { dir = "triggers"; }
-                if (r.Type == "FUNCTION"){ dir = "functions"; }
+                if (r.Type == "FUNCTION") { dir = "functions"; }
                 File.WriteAllText(
                     String.Format("{0}/{1}/{2}.sql", options.Dir, dir, r.Name),
                     r.ScriptCreate() + "\r\nGO\r\n"
                 );
             }
 
-			// TODO data
-
             Console.WriteLine("success");
-            return 0;
-		}
+        }
 	}
-        
-    class Options {
-        public string ConnString;
-        public string Dir;
-
-        public Options() { }
-        public Options(string[] args) {
-            if (args.Length > 0) ConnString = args[0];
-            if (args.Length > 1) Dir = args[1];
-
-            if (string.IsNullOrEmpty(Dir)) Dir = ".";
-        }
-
-        public List<string> Errors = new List<string>();
-        public bool Validate() {
-            Errors.Clear();
-            if (string.IsNullOrEmpty(ConnString)){
-                Errors.Add("ConnString is required.");
-            }
-            return Errors.Count == 0;
-        }
-    }
 }
--- console/Properties/AssemblyInfo.cs
+++ console/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("console")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("schemacon")]
+[assembly: AssemblyDescription("Schemanator Console")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("console")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCompany("Seth Reno")]
+[assembly: AssemblyProduct("Schemanator")]
+[assembly: AssemblyCopyright("Copyright ©  2009 Seth Reno")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
