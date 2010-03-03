25ae0aaaefb560f8c2173aad5daa645f7da257bc
sreno <sreno@49102ff3-e64e-284a-8f61-b7e53b302e95>
2010-03-03 03:10:32 +0000

    added ability to select source safe label as migration source
    git-svn-id: svn://localhost/schemanator/trunk@60 49102ff3-e64e-284a-8f61-b7e53b302e95

---
+++ vs_addin/ChooseLabel.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class ChooseLabel
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+		Me.TableLayoutPanel1 = New System.Windows.Forms.TableLayoutPanel
+		Me.OK_Button = New System.Windows.Forms.Button
+		Me.Cancel_Button = New System.Windows.Forms.Button
+		Me.ListBox1 = New System.Windows.Forms.ListBox
+		Me.Label1 = New System.Windows.Forms.Label
+		Me.TableLayoutPanel1.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'TableLayoutPanel1
+		'
+		Me.TableLayoutPanel1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+		Me.TableLayoutPanel1.ColumnCount = 2
+		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+		Me.TableLayoutPanel1.ColumnStyles.Add(New System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+		Me.TableLayoutPanel1.Controls.Add(Me.OK_Button, 0, 0)
+		Me.TableLayoutPanel1.Controls.Add(Me.Cancel_Button, 1, 0)
+		Me.TableLayoutPanel1.Location = New System.Drawing.Point(126, 206)
+		Me.TableLayoutPanel1.Name = "TableLayoutPanel1"
+		Me.TableLayoutPanel1.RowCount = 1
+		Me.TableLayoutPanel1.RowStyles.Add(New System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50.0!))
+		Me.TableLayoutPanel1.Size = New System.Drawing.Size(146, 29)
+		Me.TableLayoutPanel1.TabIndex = 0
+		'
+		'OK_Button
+		'
+		Me.OK_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+		Me.OK_Button.Location = New System.Drawing.Point(3, 3)
+		Me.OK_Button.Name = "OK_Button"
+		Me.OK_Button.Size = New System.Drawing.Size(67, 23)
+		Me.OK_Button.TabIndex = 0
+		Me.OK_Button.Text = "OK"
+		'
+		'Cancel_Button
+		'
+		Me.Cancel_Button.Anchor = System.Windows.Forms.AnchorStyles.None
+		Me.Cancel_Button.DialogResult = System.Windows.Forms.DialogResult.Cancel
+		Me.Cancel_Button.Location = New System.Drawing.Point(76, 3)
+		Me.Cancel_Button.Name = "Cancel_Button"
+		Me.Cancel_Button.Size = New System.Drawing.Size(67, 23)
+		Me.Cancel_Button.TabIndex = 1
+		Me.Cancel_Button.Text = "Cancel"
+		'
+		'ListBox1
+		'
+		Me.ListBox1.FormattingEnabled = True
+		Me.ListBox1.Location = New System.Drawing.Point(14, 39)
+		Me.ListBox1.Name = "ListBox1"
+		Me.ListBox1.Size = New System.Drawing.Size(255, 160)
+		Me.ListBox1.TabIndex = 1
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(14, 13)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(224, 13)
+		Me.Label1.TabIndex = 2
+		Me.Label1.Text = "Select the label of the previous migration point"
+		'
+		'ChooseLabel
+		'
+		Me.AcceptButton = Me.OK_Button
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.CancelButton = Me.Cancel_Button
+		Me.ClientSize = New System.Drawing.Size(284, 247)
+		Me.Controls.Add(Me.Label1)
+		Me.Controls.Add(Me.ListBox1)
+		Me.Controls.Add(Me.TableLayoutPanel1)
+		Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+		Me.MaximizeBox = False
+		Me.MinimizeBox = False
+		Me.Name = "ChooseLabel"
+		Me.ShowIcon = False
+		Me.ShowInTaskbar = False
+		Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent
+		Me.Text = "ChooseLabel"
+		Me.TableLayoutPanel1.ResumeLayout(False)
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+    Friend WithEvents TableLayoutPanel1 As System.Windows.Forms.TableLayoutPanel
+    Friend WithEvents OK_Button As System.Windows.Forms.Button
+	Friend WithEvents Cancel_Button As System.Windows.Forms.Button
+	Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+	Friend WithEvents Label1 As System.Windows.Forms.Label
+
+End Class
