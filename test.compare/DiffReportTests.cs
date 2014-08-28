b283bebfce41ab6f5cfd27e14f24c942ed0c326d
Sebastian Brandt <bastibrandt@gmail.com>
2014-08-28 09:32:37 +0200

    Added test assembly especially for compare tests

---
+++ test.compare/DiffReportTests.cs
@@ -0,0 +1,16 @@
+﻿using FluentAssertions;
+using model;
+using NUnit.Framework;
+
+namespace test.compare {
+    public class DiffReportTests {
+        [Test]
+        public void CanCreateDiffReport() {
+            var databaseDiff = new DatabaseDiff();
+
+            var report = databaseDiff.CreateDiffReport();
+
+            report.Should().NotBeNull();
+        }
+    }
+}
