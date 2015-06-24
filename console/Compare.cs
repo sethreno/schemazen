b7b3eb89b2abb7923cbacded1cb82a0c0d99c0b1
Keith Hall <kingkeith+github@gmail.com>
2015-06-24 23:44:53 +0300

    list changes found when comparing dbs

--- console/Compare.cs
+++ console/Compare.cs
@@ -10,6 +10,7 @@ namespace console {
 		private string _target;
 		private string _outDiff;
 		private bool _overwrite;
+		private bool _verbose;
 
 		public Compare() {
 			IsCommand("Compare", "Compare two databases.");
@@ -31,6 +32,10 @@ namespace console {
 				"o|overwrite=",
 				"Overwrite existing target without prompt.",
 				o => _overwrite = o != null);
+			HasOption(
+				"v|verbose=",
+				"Enable verbose mode (show detailed changes).",
+				o => _verbose = o != null);
 		}
 
 		public override int Run(string[] remainingArguments) {
@@ -43,7 +48,9 @@ namespace console {
 			DatabaseDiff diff = sourceDb.Compare(targetDb);
 			if (diff.IsDiff) {
 				Console.WriteLine("Databases are different.");
+				Console.WriteLine(diff.SummarizeChanges(_verbose));
 				if (!string.IsNullOrEmpty(_outDiff)) {
+					Console.WriteLine();
 					if (!_overwrite && File.Exists(_outDiff)) {
 						if (!ConsoleQuestion.AskYN(string.Format("{0} already exists - do you want to replace it", _outDiff))) {
 							return 1;
