96fa2837909135826d0454619af34150f81f89e8
Keith Hall <kingkeith+github@gmail.com>
2015-10-27 09:14:37 +0200

    fix non CLS complaint (unused) property

--- model/DbProperty.cs
+++ model/DbProperty.cs
@@ -1,7 +1,5 @@
 ﻿namespace SchemaZen.model {
 	public class DbProp {
-		public Database _db;
-
 		public DbProp(string name, string value) {
 			Name = name;
 			Value = value;
@@ -13,16 +11,13 @@
 		public string Script() {
 			switch (Name.ToUpper()) {
 				case "COLLATE":
-					if (string.IsNullOrEmpty(Value)) return "";
-					return string.Format("EXEC('ALTER DATABASE [' + @DB + '] COLLATE {0}')", Value);
+					return string.IsNullOrEmpty(Value) ? string.Empty : string.Format("EXEC('ALTER DATABASE [' + @DB + '] COLLATE {0}')", Value);
 
 				case "COMPATIBILITY_LEVEL":
-					if (string.IsNullOrEmpty(Value)) return "";
-					return string.Format("EXEC dbo.sp_dbcmptlevel @DB, {0}", Value);
+					return string.IsNullOrEmpty(Value) ? string.Empty : string.Format("EXEC dbo.sp_dbcmptlevel @DB, {0}", Value);
 
 				default:
-					if (string.IsNullOrEmpty(Value)) return "";
-					return string.Format("EXEC('ALTER DATABASE [' + @DB + '] SET {0} {1}')", Name, Value);
+					return string.IsNullOrEmpty(Value) ? string.Empty : string.Format("EXEC('ALTER DATABASE [' + @DB + '] SET {0} {1}')", Name, Value);
 			}
 		}
 	}
