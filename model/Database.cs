3d98c036007789dc987ec8bad704aebe1b8125d2
Dan Bradley <dbradley@globebmg.com>
2015-12-09 20:45:53 +0000

    Fix for nvarchar columns in table types

--- model/Database.cs
+++ model/Database.cs
@@ -563,7 +563,7 @@ order by fk.name, fkc.constraint_column_id
 					t.name as DATA_TYPE,
 					c.column_id as ORDINAL_POSITION,
 					CASE WHEN c.is_nullable = 1 THEN 'YES' ELSE 'NO' END as IS_NULLABLE,
-					CAST(c.max_length as int) as CHARACTER_MAXIMUM_LENGTH,
+					CASE WHEN t.name = 'nvarchar' THEN CAST(c.max_length as int)/2 ELSE CAST(c.max_length as int) END as CHARACTER_MAXIMUM_LENGTH,
 					c.precision as NUMERIC_PRECISION,
 					c.scale as NUMERIC_SCALE,
 					CASE WHEN c.is_rowguidcol = 1 THEN 'YES' ELSE 'NO' END as IS_ROW_GUID_COL
