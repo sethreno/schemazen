fe70ea61e1fd4a17b0b0ec6ff6dcdcdafc6fcff5
Eliah Hecht <eliah@zocdoc.com>
2016-02-03 13:40:02 -0500

    escape user names in case they have dots (or other such things) in them

--- test/UserTester.cs
+++ test/UserTester.cs
@@ -1,1 +1,15 @@
+﻿using NUnit.Framework;
+using SchemaZen.model;
+
+namespace SchemaZen.test {
+    [TestFixture]
+    class UserTester {
+        [Test]
+        public void TestUserNameShouldBeEscaped() {
+            var user = new SqlUser("foo.bar", "dbo");
+            var createScript = user.ScriptCreate();
+
+            StringAssert.StartsWith("CREATE USER [foo.bar]", createScript);
         }
+    }
+}
