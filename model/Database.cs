71bc8d3bd266a3e59f2356268ef83b0e7a2d141b
Keith Hall <kingkeith+github@gmail.com>
2016-02-17 16:27:49 +0200

    fix #69

--- model/Database.cs
+++ model/Database.cs
@@ -405,62 +405,67 @@ order by fk.name, fkc.constraint_column_id
 
 		private void LoadConstraintsAndIndexes(SqlCommand cm) {
 			//get constraints & indexes
-			cm.CommandText = @"
-					select 
-						s.name as schemaName,
-						t.name as tableName, 
-						t.baseType,
-						i.name as indexName, 
-						c.name as columnName,
-						i.is_primary_key, 
-						i.is_unique_constraint,
-						i.is_unique, 
-						i.type_desc,
-						isnull(ic.is_included_column, 0) as is_included_column
-					from (
-						select object_id, name, schema_id, 'T' as baseType
-						from   sys.tables
-						union
-						select object_id, name, schema_id, 'V' as baseType
-						from   sys.views
-						union
-						select type_table_object_id, name, schema_id, 'TVT' as baseType
-						from   sys.table_types
-						) t
-						inner join sys.indexes i on i.object_id = t.object_id
-						inner join sys.index_columns ic on ic.object_id = t.object_id
-							and ic.index_id = i.index_id
-						inner join sys.columns c on c.object_id = t.object_id
-							and c.column_id = ic.column_id
-						inner join sys.schemas s on s.schema_id = t.schema_id
-					where i.type_desc != 'HEAP'
-					order by s.name, t.name, i.name, ic.key_ordinal, ic.index_column_id";
-			using (var dr = cm.ExecuteReader()) {
-				while (dr.Read()) {
-					var t = (string) dr["baseType"] == "V"
-						? new Table((string) dr["schemaName"], (string) dr["tableName"])
-						: FindTable((string) dr["tableName"], (string) dr["schemaName"], ((string) dr["baseType"]) == "TVT");
-					var c = t.FindConstraint((string) dr["indexName"]);
-					if (c == null) {
-						c = new Constraint((string) dr["indexName"], "", "");
-						t.AddConstraint(c);
+			var from_sql = new[] {
+				"select object_id, name, schema_id, 'T' as baseType from sys.tables",
+				"select object_id, name, schema_id, 'V' as baseType from sys.views",
+				"select type_table_object_id as object_id, name, schema_id, 'TVT' as baseType from sys.table_types"
+			};
+			foreach (var from_statement in from_sql)
+			{
+				try {
+					cm.CommandText = @"
+						select 
+							s.name as schemaName,
+							t.name as tableName, 
+							t.baseType,
+							i.name as indexName, 
+							c.name as columnName,
+							i.is_primary_key, 
+							i.is_unique_constraint,
+							i.is_unique, 
+							i.type_desc,
+							isnull(ic.is_included_column, 0) as is_included_column
+						from (
+							" + from_statement + @"
+							) t
+							inner join sys.indexes i on i.object_id = t.object_id
+							inner join sys.index_columns ic on ic.object_id = t.object_id
+								and ic.index_id = i.index_id
+							inner join sys.columns c on c.object_id = t.object_id
+								and c.column_id = ic.column_id
+							inner join sys.schemas s on s.schema_id = t.schema_id
+						where i.type_desc != 'HEAP'
+						order by s.name, t.name, i.name, ic.key_ordinal, ic.index_column_id";
+					using (var dr = cm.ExecuteReader()) {
+						while (dr.Read()) {
+							var t = (string)dr["baseType"] == "V"
+								? new Table((string)dr["schemaName"], (string)dr["tableName"])
+								: FindTable((string)dr["tableName"], (string)dr["schemaName"], ((string)dr["baseType"]) == "TVT");
+							var c = t.FindConstraint((string)dr["indexName"]);
+							if (c == null) {
+								c = new Constraint((string)dr["indexName"], "", "");
+								t.AddConstraint(c);
 
-						if ((string) dr["baseType"] == "V")
-							ViewIndexes.Add(c);
-					}
-					c.Clustered = (string) dr["type_desc"] == "CLUSTERED";
-					c.Unique = (bool) dr["is_unique"];
-					if ((bool) dr["is_included_column"]) {
-						c.IncludedColumns.Add((string) dr["columnName"]);
-					} else {
-						c.Columns.Add((string) dr["columnName"]);
-					}
+								if ((string)dr["baseType"] == "V")
+									ViewIndexes.Add(c);
+							}
+							c.Clustered = (string)dr["type_desc"] == "CLUSTERED";
+							c.Unique = (bool)dr["is_unique"];
+							if ((bool)dr["is_included_column"]) {
+								c.IncludedColumns.Add((string)dr["columnName"]);
+							} else {
+								c.Columns.Add((string)dr["columnName"]);
+							}
 
-					c.Type = "INDEX";
-					if ((bool) dr["is_primary_key"])
-						c.Type = "PRIMARY KEY";
-					if ((bool) dr["is_unique_constraint"])
-						c.Type = "UNIQUE";
+							c.Type = "INDEX";
+							if ((bool)dr["is_primary_key"])
+								c.Type = "PRIMARY KEY";
+							if ((bool)dr["is_unique_constraint"])
+								c.Type = "UNIQUE";
+						}
+					}
+				} catch (SqlException) {
+					// SQL server version doesn't support table types, nothing to do here
 				}
 			}
 		}
