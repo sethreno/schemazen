ac960ad3fe67c8f5fef7f8d2bf8745c13fbd4185
Colin Hanson <colin.hanson@zocdoc.com>
2016-03-18 14:40:50 -0400

    Add support for roles

--- model/Database.cs
+++ model/Database.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Web.Security;
 
 namespace SchemaZen.model {
 	public class Database {
@@ -70,7 +71,8 @@ namespace SchemaZen.model {
 		public List<Synonym> Synonyms = new List<Synonym>();
 		public List<Table> TableTypes = new List<Table>();
 		public List<Table> Tables = new List<Table>();
-		public List<SqlUser> Users = new List<SqlUser>();
+        public List<Role> Roles = new List<Role>();
+        public List<SqlUser> Users = new List<SqlUser>();
 		public List<Constraint> ViewIndexes = new List<Constraint>();
 
 		public DbProp FindProp(string name) {
@@ -121,7 +123,7 @@ namespace SchemaZen.model {
 
 		private static readonly string[] dirs = {
 			"tables", "foreign_keys", "assemblies", "functions", "procedures", "triggers",
-			"views", "xmlschemacollections", "data", "users", "synonyms", "table_types"
+			"views", "xmlschemacollections", "data", "users", "synonyms", "table_types", "roles"
 		};
 
 		private void SetPropOnOff(string propName, object dbVal) {
@@ -148,6 +150,7 @@ namespace SchemaZen.model {
 			Assemblies.Clear();
 			Users.Clear();
 			Synonyms.Clear();
+            Roles.Clear();
 
 			using (var cn = new SqlConnection(Connection)) {
 				cn.Open();
@@ -167,11 +170,12 @@ namespace SchemaZen.model {
 					LoadCLRAssemblies(cm);
 					LoadUsersAndLogins(cm);
 					LoadSynonyms(cm);
-				}
+                    LoadRoles(cm);
+                }
 			}
 		}
 
-		private void LoadSynonyms(SqlCommand cm) {
+	    private void LoadSynonyms(SqlCommand cm) {
 			try {
 				// get synonyms
 				cm.CommandText = @"
@@ -189,7 +193,125 @@ namespace SchemaZen.model {
 			}
 		}
 
-		private void LoadUsersAndLogins(SqlCommand cm) {
+        private void LoadRoles(SqlCommand cm) {
+            cm.CommandText = @"
+create table #ScriptedRoles (
+	name nvarchar(255) not null
+,	script nvarchar(max)
+)
+
+insert into #ScriptedRoles
+select 
+	name
+,	null as script 
+from sys.database_principals
+where type = 'R'
+	and name not in (
+	-- Ignore default roles, just look for custom ones
+		'db_accessadmin'
+	,	'db_backupoperator'
+	,	'db_datareader'
+	,	'db_datawriter'
+	,	'db_ddladmin'
+	,	'db_denydatareader'
+	,	'db_denydatawriter'
+	,	'db_owner'
+	,	'db_securityadmin'
+	,	'public'
+	)
+
+while(exists(select 1 from #ScriptedRoles where script is null))
+begin
+
+	DECLARE @RoleName VARCHAR(255)
+	SET @RoleName = (select top 1 name from #ScriptedRoles where script is null)
+
+	-- Script out the Role
+	DECLARE @roleDesc VARCHAR(MAX), @crlf VARCHAR(2)
+	SET @crlf = CHAR(13) + CHAR(10)
+	SET @roleDesc = 'CREATE ROLE [' + @roleName + ']' + @crlf + 'GO' + @crlf + @crlf
+
+	SELECT    @roleDesc = @roleDesc +
+			CASE dp.state
+				WHEN 'D' THEN 'DENY '
+				WHEN 'G' THEN 'GRANT '
+				WHEN 'R' THEN 'REVOKE '
+				WHEN 'W' THEN 'GRANT '
+			END + 
+			dp.permission_name + ' ' +
+			CASE dp.class
+				WHEN 0 THEN ''
+				WHEN 1 THEN --table or column subset on the table
+					CASE WHEN dp.major_id < 0 THEN
+						+ 'ON [sys].[' + OBJECT_NAME(dp.major_id) + '] '
+					ELSE
+						+ 'ON [' +
+						(SELECT SCHEMA_NAME(schema_id) + '].[' + name FROM sys.objects WHERE object_id = dp.major_id)
+							+ -- optionally concatenate column names
+						CASE WHEN MAX(dp.minor_id) > 0 
+							 THEN '] ([' + REPLACE(
+											(SELECT name + '], [' 
+											 FROM sys.columns 
+											 WHERE object_id = dp.major_id 
+												AND column_id IN (SELECT minor_id 
+																  FROM sys.database_permissions 
+																  WHERE major_id = dp.major_id
+																	AND USER_NAME(grantee_principal_id) IN (@roleName)
+																 )
+											 FOR XML PATH('')
+											) --replace final square bracket pair
+										+ '])', ', []', '')
+							 ELSE ']'
+						END + ' '
+					END
+				WHEN 3 THEN 'ON SCHEMA::[' + SCHEMA_NAME(dp.major_id) + '] '
+				WHEN 4 THEN 'ON ' + (SELECT RIGHT(type_desc, 4) + '::[' + name FROM sys.database_principals WHERE principal_id = dp.major_id) + '] '
+				WHEN 5 THEN 'ON ASSEMBLY::[' + (SELECT name FROM sys.assemblies WHERE assembly_id = dp.major_id) + '] '
+				WHEN 6 THEN 'ON TYPE::[' + (SELECT name FROM sys.types WHERE user_type_id = dp.major_id) + '] '
+				WHEN 10 THEN 'ON XML SCHEMA COLLECTION::[' + (SELECT SCHEMA_NAME(schema_id) + '.' + name FROM sys.xml_schema_collections WHERE xml_collection_id = dp.major_id) + '] '
+				WHEN 15 THEN 'ON MESSAGE TYPE::[' + (SELECT name FROM sys.service_message_types WHERE message_type_id = dp.major_id) + '] '
+				WHEN 16 THEN 'ON CONTRACT::[' + (SELECT name FROM sys.service_contracts WHERE service_contract_id = dp.major_id) + '] '
+				WHEN 17 THEN 'ON SERVICE::[' + (SELECT name FROM sys.services WHERE service_id = dp.major_id) + '] '
+				WHEN 18 THEN 'ON REMOTE SERVICE BINDING::[' + (SELECT name FROM sys.remote_service_bindings WHERE remote_service_binding_id = dp.major_id) + '] '
+				WHEN 19 THEN 'ON ROUTE::[' + (SELECT name FROM sys.routes WHERE route_id = dp.major_id) + '] '
+				WHEN 23 THEN 'ON FULLTEXT CATALOG::[' + (SELECT name FROM sys.fulltext_catalogs WHERE fulltext_catalog_id = dp.major_id) + '] '
+				WHEN 24 THEN 'ON SYMMETRIC KEY::[' + (SELECT name FROM sys.symmetric_keys WHERE symmetric_key_id = dp.major_id) + '] '
+				WHEN 25 THEN 'ON CERTIFICATE::[' + (SELECT name FROM sys.certificates WHERE certificate_id = dp.major_id) + '] '
+				WHEN 26 THEN 'ON ASYMMETRIC KEY::[' + (SELECT name FROM sys.asymmetric_keys WHERE asymmetric_key_id = dp.major_id) + '] '
+			 END COLLATE SQL_Latin1_General_CP1_CI_AS
+			 + 'TO [' + @roleName + ']' + 
+			 CASE dp.state WHEN 'W' THEN ' WITH GRANT OPTION' ELSE '' END + @crlf
+	FROM    sys.database_permissions dp
+	WHERE    USER_NAME(dp.grantee_principal_id) IN (@roleName)
+	GROUP BY dp.state, dp.major_id, dp.permission_name, dp.class
+
+	update #ScriptedRoles 
+	set script = @roleDesc
+	where name = @RoleName
+
+	print @roleDesc
+
+end
+
+select 
+    name
+,   script
+from #ScriptedRoles
+";
+            Role r = null;
+            using (var dr = cm.ExecuteReader())
+            {
+                while (dr.Read()) {
+                    r = new Role {
+                        Name = (string) dr["name"],
+                        Script = (string) dr["script"]
+                    };
+                    Roles.Add(r);
+                }
+            }
+        }
+
+        private void LoadUsersAndLogins(SqlCommand cm) {
 			// get users that have access to the database
 			cm.CommandText = @"
 				select dp.name as UserName, USER_NAME(drm.role_principal_id) as AssociatedDBRole, default_schema_name
@@ -1043,7 +1165,8 @@ where name = @dbname
 			}
 			WriteScriptDir("views", ViewIndexes.ToArray(), log);
 			WriteScriptDir("assemblies", Assemblies.ToArray(), log);
-			WriteScriptDir("users", Users.ToArray(), log);
+            WriteScriptDir("roles", Roles.ToArray(), log);
+            WriteScriptDir("users", Users.ToArray(), log);
 			WriteScriptDir("synonyms", Synonyms.ToArray(), log);
 
 			ExportData(tableHint, log);
---
+++ model/Role.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchemaZen.model
+{
+    public class Role : IScriptable, INameable
+    {
+        public string Name { get; set; }
+        public string Script { get; set; }
+
+        public string ScriptCreate() {
+            return Script;
+        }
+    }
+}
