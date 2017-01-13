6c34b00de851050b31581e9295f83c97798c725f
Colton Correll <colton.c.correll@gmail.com>
2017-01-13 10:06:53 -0600

    fix unit tests failing due to inconsistent line endings

--- test/AssemblyTester.cs
+++ test/AssemblyTester.cs
@@ -5,19 +5,16 @@ using SchemaZen.Library.Models;
 namespace SchemaZen.Tests {
 	[TestFixture]
 	public class AssemblyTester {
+		[Test]
+		[TestCase("SAFE_ACCESS", "SAFE")]
+		[TestCase("UNSAFE_ACCESS", "UNSAFE")]
+		[TestCase("EXTERNAL_ACCESS", "EXTERNAL_ACCESS")]
+		public void Assembly_WithPermissionSetCases(string permissionSet, string scriptedPermissionSet) {
+			var assembly = new SqlAssembly(permissionSet, "SchemazenAssembly");
+			assembly.Files.Add(new KeyValuePair<string, byte[]>("mydll", new byte[0]));
 
-        [Test]
-        [TestCase("SAFE_ACCESS", "SAFE")]
-        [TestCase("UNSAFE_ACCESS", "UNSAFE")]
-        [TestCase("EXTERNAL_ACCESS", "EXTERNAL_ACCESS")]
-        public void Assembly_WithPermissionSetCases(string permissionSet, string scriptedPermissionSet) {
-            var assembly = new SqlAssembly(permissionSet, "SchemazenAssembly");
-            assembly.Files.Add(new KeyValuePair<string, byte[]>("mydll", new byte[0]));
-
-		    var expected = @"CREATE ASSEMBLY [SchemazenAssembly]
-FROM 0x
-WITH PERMISSION_SET = " + scriptedPermissionSet;
-		    Assert.AreEqual(expected, assembly.ScriptCreate());
+			var expected = "CREATE ASSEMBLY [SchemazenAssembly]\r\nFROM 0x\r\nWITH PERMISSION_SET = " + scriptedPermissionSet;
+			Assert.AreEqual(expected, assembly.ScriptCreate());
 		}
 	}
 }
