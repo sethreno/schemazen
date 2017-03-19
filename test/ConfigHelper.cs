ddd5ea1557c33eeb8a655bfc2131aa359820629a
Claudio Barca <c.barca@libero.it>
2017-03-19 18:11:20 +0100

    Add AssemblyDirectory to avoid test_schema_dir configuration key.

--- test/ConfigHelper.cs
+++ test/ConfigHelper.cs
@@ -1,17 +1,26 @@
 ﻿using System;
 using System.Configuration;
+using System.IO;
 
 namespace SchemaZen.Tests {
 	public class ConfigHelper {
 		public static string TestDB => GetSetting("testdb");
 
-		public static string TestSchemaDir => GetSetting("test_schema_dir");
+		public static string TestSchemaDir => Path.Combine(AssemblyDirectory, "test_schemas");
 
-		public static string SqlDbDiffPath => GetSetting("SqlDbDiffPath");
+        public static string SqlDbDiffPath => GetSetting("SqlDbDiffPath");
 
 		private static string GetSetting(string key) {
 			var val = Environment.GetEnvironmentVariable(key);
 			return val ?? ConfigurationManager.AppSettings[key];
 		}
-	}
+
+        protected internal static string AssemblyDirectory
+        {
+            get
+            {
+                return AppDomain.CurrentDomain.BaseDirectory;
+            }
+        }
+    }
 }
