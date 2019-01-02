ca9eaf1387517a30befc7231f5549ee8d0538258
Brett Martensen <brett.martensen@docusign.com>
2019-01-02 17:05:05 -0600

    Changing user export to support windows authentication in addition to sql authentication by allowing the dp.type_desc of the database principle to be 'WINDOWS_USER' in addition to 'SQL_User'.  Still filtering out guest and dbo by checking that dp.sid doesn't match 0x00 or 0x01 but added a check by name because I came across a case where my dbo user had a different value.  Implications of these changes should be minimal as all current filters/criteria still exist and additional filters/criteria will only affect users who have data that will benefit from these changes.

--- model/Models/Database.cs
+++ model/Models/Database.cs
@@ -335,8 +335,8 @@ from #ScriptedRoles
 				select dp.name as UserName, USER_NAME(drm.role_principal_id) as AssociatedDBRole, default_schema_name
 				from sys.database_principals dp
 				left outer join sys.database_role_members drm on dp.principal_id = drm.member_principal_id
-				where dp.type_desc = 'SQL_USER'
-				and dp.sid not in (0x00, 0x01) --ignore guest and dbo
+				where (dp.type_desc = 'SQL_USER' or dp.type_desc = 'WINDOWS_USER')
+				and dp.sid not in (0x00, 0x01) and dp.name not in ('dbo', 'guest')
 				and dp.is_fixed_role = 0
 				order by dp.name";
 			SqlUser u = null;
