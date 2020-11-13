1151c5ab5ba8b40e2d6ceb6440429957bb9ff40f
Fabián Foos <fabian.foos@diveria.com>
2020-11-13 11:59:15 -0300

    Fulltext catalogs and indexes scripting and comparison new feature.

---
+++ model/Models/FulltextCatalog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchemaZen.Library.Models {
+	public class FulltextCatalog : INameable, IScriptable {
+		public FulltextCatalog(string name) {
+			Name = name;
+		}
+
+		public string Name { get; set; }
+		public bool IsAccentSensitivityOn { get; set; }
+		public bool IsDefault { get; set; }
+
+		public string ScriptCreate() {
+			var text = new StringBuilder();
+			text.AppendLine(
+				$"CREATE FULLTEXT CATALOG [{Name}]{(IsAccentSensitivityOn ? " WITH ACCENT_SENSITIVITY ON" : string.Empty)}{(IsDefault ? " AS DEFAULT" : string.Empty)};");
+
+			return text.ToString();
+		}
+
+		public string ScriptDrop() {
+			var text = new StringBuilder();
+			text.AppendLine(
+				$"DROP FULLTEXT CATALOG [{Name}]");
+
+			return text.ToString();
+		}
+	}
+}
---
+++ model/Models/FulltextIndexColumn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SchemaZen.Library.Models {
+	public class FulltextIndexColumn {
+		public string ColumnName { get; set; }
+		public bool StatisticalSemantics { get; set; }
+		public int LanguageId { get; set; }
+
+		public string ScriptCreate() {
+			return $"    [{ColumnName}] LANGUAGE {LanguageId}{(StatisticalSemantics ? " STATISTICAL_SEMANTICS" : string.Empty)}";
+		}
+	}
+}
